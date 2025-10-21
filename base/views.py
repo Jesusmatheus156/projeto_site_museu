ccfd100f86e8c0e8a8289ab49aa43a6b67edb829
PhzzX0 <paulopedrohof@gmail.com>
2025-10-21 20:50:45 -0300

    Teste: FullCalendar mostrando horários ocupados e livres

--- base/views.py
+++ base/views.py
@@ -1,13 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 
-# Create your views here.
+from usuario.models import Usuario, Agendamento
 
 def base(request):
-    '''essa  funcao esta trabalhando com a requisicao da pagina e a gente tem que tratar essa requisicao'''
     return render(request, 'base/home.html')
 
 def visitar(request):
-    return render(request, 'base/visitar.html')
+    usuario = None
+    usuario_id = request.session.get('usuario_id')
+    if usuario_id:
+        try:
+            usuario = Usuario.objects.get(id=usuario_id)
+        except Usuario.DoesNotExist:
+            usuario = None
+
+    if request.method == 'POST':
+        telefone = request.POST.get('telefone')
+        tipo_visitante = request.POST.get('tipo_visitante')
+        instituicao = request.POST.get('instituicao', None)
+        quantidade = request.POST.get('quantidade_visitantes', 1)
+        data = request.POST.get('data')
+        horario = request.POST.get('horario')
+
+        if tipo_visitante == 'individual':
+            quantidade = 1
+
+        Agendamento.objects.create(
+            usuario=usuario,
+            telefone=telefone,
+            tipo_visitante=tipo_visitante,
+            instituicao=instituicao if tipo_visitante == 'instituicao' else None,
+            quantidade_visitantes=quantidade,
+            data=data,
+            horario=horario
+        )
+
+        messages.success(request, f"Agendamento realizado com sucesso para {data} às {horario}!")
+        return redirect('visitar')
+
+    # Monta o dicionário de horários ocupados para o FullCalendar
+    agendamentos = Agendamento.objects.all()
+    agendamentos_dict = {}
+    for ag in agendamentos:
+        str_data = ag.data.strftime('%Y-%m-%d')
+        if str_data not in agendamentos_dict:
+            agendamentos_dict[str_data] = []
+        agendamentos_dict[str_data].append(ag.horario.strftime('%H:%M'))
+
+    agendamentos_json = json.dumps(agendamentos_dict, cls=DjangoJSONEncoder)
+
+    context = {
+        'usuario': usuario,
+        'agendamentos_json': agendamentos_json
+    }
+    return render(request, 'base/visitar.html', context)
 
 def museu(request):
     return render(request, 'base/museu.html')
--- projeto_museu/settings.py
+++ projeto_museu/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'projeto_museu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
+}
 
 
 '''# banco de dados usando MySQL
