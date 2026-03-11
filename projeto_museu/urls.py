cd20ed7d684806d006fa9b07ef8bc6cf0ec02700
Matheus de Jesus <matheusfreire15rn@gmail.com>
2026-03-11 18:14:45 -0300

    ajeitando traducao

--- projeto_museu/urls.py
+++ projeto_museu/urls.py
@@ -4,22 +4,20 @@ from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.conf import settings
 
+# 1. ROTAS FIXAS (Não mudam de idioma e não ganham /en-us/ na URL)
 urlpatterns = [
+    # Rota para o sistema de troca de idioma do Django
+    path('i18n/', include('django.conf.urls.i18n')), 
+]
+
+# 2. ROTAS TRADUZIDAS (Ganham o prefixo /pt-br/, /en-us/ ou /es/)
+urlpatterns += i18n_patterns(
     # Interface administrativa
     path('admin/', admin.site.urls),
     # Rota para o arquivo urls.py da aplicação base
     path('', include('base.urls')),
-    # Rota para o arquivo urls.py da aplicação usuario
-    # Rota para o sistema de tradução do Django
-    path('i18n/', include('django.conf.urls.i18n')), # <-- Rota para o Django processar a troca
-]
-
-# Opcional: Para incluir o código do idioma na URL (ex: /en/home/)
-urlpatterns += i18n_patterns(
-    path('', include('base.urls')),
-
 )
 
-# ESSENCIAL: Serve arquivos de MÍDIA no modo de desenvolvimento
+# 3. ESSENCIAL: Serve arquivos de MÍDIA no modo de desenvolvimento
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
