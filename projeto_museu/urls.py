09a17bdd4e3c9ed407e6d96fbaafbc7ea41f8468
Matheus de Jesus <matheusfreire15rn@gmail.com>
2026-03-12 13:13:36 -0300

    correção de rotas de traducao

--- projeto_museu/urls.py
+++ projeto_museu/urls.py
@@ -1,20 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
-from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
 
-# Rotas que NÃO mudam com o idioma
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
+    # Interface administrativa
+    path('admin/', admin.site.urls),
+    # Rota para o arquivo urls.py da aplicação base
+    path('', include('base.urls')),
+    # Rota para o arquivo urls.py da aplicação usuario
+    # Rota para o sistema de tradução do Django
+    path('i18n/', include('django.conf.urls.i18n')), # <-- Rota para o Django processar a troca
 ]
 
-# Rotas que mudam (ganham /pt-br/, /en/, /es/)
+# Opcional: Para incluir o código do idioma na URL (ex: /en/home/)
 urlpatterns += i18n_patterns(
-    path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    prefix_default_language=True # Isso ajuda a evitar confusão no redirecionamento
 )
 
+# ESSENCIAL: Serve arquivos de MÍDIA no modo de desenvolvimento
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
